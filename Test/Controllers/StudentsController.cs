610efc9ae6fea5ea00f610e73b9e9c73b0335abf
unknown <slayerproton@gmail.com>
2023-02-15 18:20:30 +0700

    first commit

---
+++ Test/Controllers/StudentsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AuthoEx.Data;
+using AuthoEx.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthoEx.Controllers
+{
+
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Students
+        [Authorize(Policy = "CrudData")]
+        [Authorize(Policy = "AddAndView")]
+        [Authorize(Policy = "View")]
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Students.ToListAsync());
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        [Authorize(Policy = "CrudData")]
+        [Authorize(Policy = "AddAndView")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "CrudDatag")]
+        [Authorize(Policy = "AddAndView")]
+        public async Task<IActionResult> Create([Bind("StudentId,Name,DateOfBirth,Email,Address")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        [Authorize(Policy = "CrudData")]
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "CrudData")]
+        public async Task<IActionResult> Edit(long id, [Bind("StudentId,Name,DateOfBirth,Email,Address")] Student student)
+        {
+            if (id != student.StudentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.StudentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        [Authorize(Policy = "CrudData")]
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "CrudData")]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Students == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Students'  is null.");
+            }
+            var student = await _context.Students.FindAsync(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(long id)
+        {
+          return _context.Students.Any(e => e.StudentId == id);
+        }
+    }
+}
